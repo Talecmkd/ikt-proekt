8eb8c6586a3cc3b6fe03c6703a795676197a68d0
Kire Atanaskov <atanaskovkire@gmail.com>
2025-03-27 18:49:44 +0100

    Initial commit

---
+++ src/main/java/finki/ikt/iktproekt/IktProektApplication.java
@@ -0,0 +1,13 @@
+package finki.ikt.iktproekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IktProektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IktProektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/finki/ikt/iktproekt/IktProektApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.ikt.iktproekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IktProektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
