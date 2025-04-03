bff15706ae51dadaa1f11efffeb4ab024df28a8e
Renata Peeva <renatapeeva4@gmail.com>
2025-04-03 22:44:16 +0200

    Implemented REST endpoint POST /api/quizzes to create a quiz and GET/api/quizzes/{id} to retrieve a quiz by id

---
+++ src/main/java/finki/ikt/iktproekt/controller/QuizController.java
@@ -0,0 +1,37 @@
+package finki.ikt.iktproekt.controller;
+
+import finki.ikt.iktproekt.Service.QuizService;
+import finki.ikt.iktproekt.model.Quiz;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/api/quizzes")
+public class QuizController {
+
+    private final QuizService quizService;
+
+    public QuizController(QuizService quizService) {
+        this.quizService = quizService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> createQuiz(@Valid @RequestBody Quiz quiz) {
+        Quiz createdQuiz = quizService.create(quiz);
+        return ResponseEntity.created(URI.create("/api/quizzes/" + createdQuiz.getQuiz_id()))
+                .body(createdQuiz.getQuiz_id());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Quiz> getQuizById(@PathVariable Long id) {
+        Optional<Quiz> quiz = quizService.findById(id);
+        return quiz.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
